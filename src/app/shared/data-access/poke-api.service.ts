4693e0fe9306cb2ca44135d3d7ae05bd05479a99
André Figueirôa <alfigueiroab@gmail.com>
2023-03-09 22:02:12 -0300

    Integrate with API

--- src/app/shared/data-access/poke-api.service.ts
+++ src/app/shared/data-access/poke-api.service.ts
@@ -1,8 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable, of } from 'rxjs';
 import { ListResult } from '../models/list-result.model';
 import { Pokemon } from '../models/pokemon.model';
-import PokemonDataJson from './pokemon-data.json';
 
 export const PAGE_SIZE = 20;
 export const POKEMON_MAX_AMOUNT = 151;
@@ -12,16 +12,15 @@ export const POKEMON_MAX_AMOUNT = 151;
 })
 export class PokeApiService {
 
+  constructor(private http: HttpClient) {}
+
   getPokemonList(page: number = 0, searchText: string = ''): Observable<ListResult<Pokemon>> {
-    return of(PokemonDataJson as Pokemon[]).pipe(
-      map<Pokemon[], ListResult<Pokemon>>(pokemonList => {
-        const filteredList = pokemonList.filter(pkm => pkm.name.toLowerCase().includes(searchText.toLowerCase()));
-        const maxAmount = filteredList.length;
-        return {
-          last: PAGE_SIZE * page + PAGE_SIZE >= maxAmount,
-          content: filteredList.slice(PAGE_SIZE * page, PAGE_SIZE * page + PAGE_SIZE)
-        }
+    return this.http.get<ListResult<Pokemon>>('http://localhost:3000/pokemon', {
+      params: {
+        page,
+        searchText
       }
-    ));
+    })
   }
+
 }
